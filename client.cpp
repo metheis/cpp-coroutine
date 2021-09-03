15679536835ca79cea8d6c32721c80228fdcfd89
metheis <markt@berkeley.edu>
2021-09-03 01:32:17 -0700

    modify client to make coroutine longer

--- client.cpp
+++ client.cpp
@@ -51,6 +51,7 @@ counter()
 {
   auto pp = co_await GetPromise<ReturnObject3::promise_type>{};
   printf("hello world\n");
+  char temp[] = "hello world 1234567890 hello hello";
   // for (unsigned i = 0;; ++i) {
   //   pp->value_ = i;
   //   co_await std::suspend_always{};
@@ -62,7 +63,6 @@ int main(int argc, char const *argv[])
   int sock = 0, valread, ret;
   struct sockaddr_in serv_addr;
   const char *hello = "Hello from client";
-  char buffer[1024] = {0};
   if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
   {
     printf("\n Socket creation error \n");
@@ -86,10 +86,13 @@ int main(int argc, char const *argv[])
   }
   std::cout << "size of handle: " << sizeof(std::coroutine_handle<ReturnObject3::promise_type>) << std::endl;
   std::coroutine_handle<ReturnObject3::promise_type> h = counter();
+  std::cout << "size of h: " << sizeof(h) << std::endl;
+  char temp[] = "hello world 1234567890 hello hello";
+  std::cout << "size of temp: " << sizeof(temp) << std::endl;
   // print return object
   unsigned char *p1 = (unsigned char *)h.address();
   int j;
-  std::cout << "original handle: " << std::endl;
+  std::cout << "original h handle: " << std::endl;
   for (j = 0; j < sizeof(std::coroutine_handle<ReturnObject3::promise_type>); j++)
   {
     printf("%02hhX ", p1[j]);
@@ -97,6 +100,9 @@ int main(int argc, char const *argv[])
   printf("\n");
   // ReturnObject3::promise_type &promise = h.promise();
   // std::cout << "counter: " << promise.value_ << std::endl;
+  
+  // h();
+
   if ((ret = write(sock, h.address(), sizeof(std::coroutine_handle<ReturnObject3::promise_type>))) == -1)
   {
     perror("write() error");
@@ -105,12 +111,38 @@ int main(int argc, char const *argv[])
   // print out what was sent
   unsigned char *p = (unsigned char *)h.address();
   int i;
-  std::cout << "sent handle: " << std::endl;
+  std::cout << "h sent handle: " << std::endl;
   for (i = 0; i < sizeof(std::coroutine_handle<ReturnObject3::promise_type>); i++)
   {
     printf("%02hhX ", p[i]);
   }
   printf("\n");
+  char buffer[10] = {0};
+  memcpy(buffer, h.address(), sizeof(std::coroutine_handle<ReturnObject3::promise_type>));
+  std::coroutine_handle<ReturnObject3::promise_type> h2;
+  h2 = std::coroutine_handle<ReturnObject3::promise_type>::from_address(&buffer);
+  ReturnObject3::promise_type &promise = h2.promise();
+  // print out what was received
+  unsigned char *p2 = (unsigned char *)h2.address();
+  int k;
+  std::cout << "h2 handle: " << std::endl;
+  for (k = 0; k < sizeof(std::coroutine_handle<ReturnObject3::promise_type>); k++)
+  {
+    printf("%02hhX ", p2[k]);
+  }
+  printf("\n");
+  h2();
+  // p = (unsigned char *)h.address();
+  // std::cout << "h after run handle: " << std::endl;
+  // for (i = 0; i < sizeof(std::coroutine_handle<ReturnObject3::promise_type>); i++)
+  // {
+  //   printf("%02hhX ", p[i]);
+  // }
+  // printf("\n");
+  // h();
+  // ReturnObject3::promise_type &promise = h.promise();
+  // std::cout << "counter: " << promise.value_ << std::endl;
+  // h2();
   // h.destroy();
   // printf("routine message sent\n");
   return 0;
